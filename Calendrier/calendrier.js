1c488a3d2f6b2ba2f6f61146a2528df2f8d9db38
pof-cpu <63476556+pof-cpu@users.noreply.github.com>
2021-07-10 21:11:11 -1000

    1

--- Calendrier/calendrier.js
+++ Calendrier/calendrier.js
@@ -1,7 +1,23 @@
 // let today = new Date()
 
-let today = new Date(2021,0,1);
+let today = new Date(2021,3,10);
+let aujourdhui = new Date()
 
+
+function WithoutTime(dateTime) {
+    let date = new Date(dateTime.getTime());
+    date.setHours(0, 0, 0, 0);
+    return date;
+}
+
+
+// console.log('aujourd\'hui :' + aujourdhui);
+// console.log('today :' + today);
+// console.log(aujourdhui.getTime()==today.getTime());
+
+
+
+// console.log(aujourdhui.getDate());
 let aa = today.getFullYear();
 let mm = today.getMonth();
 let jj = today.getDate();
@@ -13,6 +29,10 @@ let datelocalelongue = today.toLocaleDateString('fr-FR',{
     month:'long',
     day: 'numeric'
 });
+
+aujourdhui = WithoutTime(aujourdhui);
+today = WithoutTime(today);
+
 
 // retourne le mois en texte
 function texte_mois (mydate){
@@ -27,10 +47,12 @@ document.getElementById('datelocalelongue').innerHTML=
     'On considère la date : '+datelocalelongue + 
     '<br>Calendrier du mois de ' + texte_mois(today);
 
+ 
+
 // fonction retournant le 1er jour
 // -------------------------------------
 function premierjour(mydate){
-    return mydate.getDay();
+    return new Date(mydate.getFullYear(), mydate.getMonth(), 1).getDay();
 }
 // fonction retournant le Dernier jour
 // -------------------------------------
@@ -43,32 +65,66 @@ function dernierjourmoisprecedant(mydate){
     return new Date(mydate.getFullYear(), mydate.getMonth(),0).getDate();
 }
 
+// Jours fériés de Avril
+const avrilferie=[2,4,5]
+
+
+
 // Remplissage du calendrier
 //----------------------------------------
+document.getElementById('mois').innerHTML=texte_mois(today) +' '+ today.getFullYear();
+
 let d = document.querySelectorAll(".jour");
 let jour = 0;
 let pj = premierjour(today);
 let dj = dernierjour(today);
 let djp= dernierjourmoisprecedant(today);
+let contenu =0;
 // let pdj = precdernierjour(today);
+let saut=0;
 
 console.log('Mois en cours ' + texte_mois(today));
 console.log('premier jour ' + pj);
 console.log('dernier jour ' + dj);
 console.log('dernier mois précédent ' + djp);
 
+jour = 2-premierjour(today)
+console.log('jour '+jour);
+if (jour==1){
+    saut=-7;
+}else{
+    saut=0;
+}
+ 
 
 for(var i = 0; i< d.length;i++){
-    jour=i-premierjour(today)+2;
-    if (jour<1){
-    d[i].innerHTML=djp-(7-pj)+i+1;
+    jour=i-premierjour(today)+2+saut;
+
+    //mois précédent
+    if (jour<1){   
+        contenu=djp+jour;
+        d[i].innerHTML="<span style=\"color:gray\">" + contenu + "</span>";
     
     }
+    //mois en cours
     if(0<jour && jour<(dj+1)){
+        //aujourd'hui
+        if (aujourdhui.getTime()==today.getTime()){
+            if (jour==today.getDate()){
+            d[i].innerHTML="<div style=\"box-shadow: 0 0 7px 7px #0011ff83;\">" + jour + "</div>";
+            }else {
+            d[i].innerHTML=jour;
+        }
+    }else{
         d[i].innerHTML=jour;
     }
+
+    }
+    //mois suivant
     if (jour> dj){
-        d[i].innerHTML=jour-dj;
+        contenu=jour-dj;
+        d[i].innerHTML="<span style=\"color:gray\">" + contenu + "</span>";
+        
     }
 }
 
