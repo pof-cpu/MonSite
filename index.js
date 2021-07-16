3be92ac0b63d3c7cad3be5388618cf04e41e82ef
pof-cpu <63476556+pof-cpu@users.noreply.github.com>
2021-07-15 21:22:52 -1000

    1

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ burger.addEventListener('click',()=>{
     console.log('ok');
     navlink.classList.toggle('not-visible');
 })
+
+let item1 = document.getElementById("item1");
+let item2 = document.getElementById("item2");
+let item3 = document.getElementById("item3");
+let item4 = document.getElementById("item4");
+let item5 = document.getElementById("item5");
+
+item1.onclick=function(){
+    location.href="index.html";
+};
+item2.onclick=function(){
+    location.href="python.html";
+};
+// item2.addEventListener('mouseover',()=>{
+//     item2.classList.toggle('survole');
+// });
