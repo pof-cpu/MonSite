28421c53eb91f50caffa948392b82e01cd944ea4
pof-cpu <63476556+pof-cpu@users.noreply.github.com>
2021-07-21 16:24:03 -1000

    1

--- test/SideBar/sidebar.js
+++
@@ -1,9 +0,0 @@
-console.log('javascript chargé')
-
-let btn =document.getElementById('btn')
-// let btn =document.querySelector("#btn");
-let sidebar =document.querySelector(".sidebar");
-
-btn.onclick=function(){
-    sidebar.classList.toggle('active');
-}
